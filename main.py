1ceee65e7b03ae441d8f8c5397ac26ffe0375678
Yucheng Zhu <YZ_YuchengZhu@outlook.com>
2022-04-10 02:09:43 +0800

    delete obsolete files

--- main.py
+++
@@ -1,7 +0,0 @@
-from Detector import *
-
-detector = Detector(model_type='OD')
-
-# detector.onImage("data/img/test/0.png")
-# detector.onImage("data/img/people1.jpg")
-detector.onVideo("data/videos/Les filles de Poutine sanctionnées.mp4")
